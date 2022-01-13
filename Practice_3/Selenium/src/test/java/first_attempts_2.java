7feee4e4f3c1ef9455039c9fed97ed1e769209ff
SSanich <sanich.grig@gmail.com>
2022-01-13 11:33:28 +0700

    selenium test

--- Practice_3/Selenium/src/test/java/first_attempts_2.java
+++ Practice_3/Selenium/src/test/java/first_attempts_2.java
@@ -2,11 +2,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-
 import java.util.concurrent.TimeUnit;
 
 
@@ -15,27 +13,26 @@ public class first_attempts_2 {
     private WebDriver driver;
     private String baseUrl;
     private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
-    JavascriptExecutor js;
+
 
     @Before
     public void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "");
+        System.setProperty("webdriver.chrome.driver", "src/test/java/first_attempts_2.java");
         driver = new ChromeDriver();
-        baseUrl = "https://www.google.com/";
+        baseUrl = "https://sandbox.cardpay.com/MI/cardpayment2.html?orderXml=PE9SREVSIFdBTExFVF9JRD0nODI5OScgT1JERVJfTlVNQkVSPSc0NTgyMTEnIEFNT1VOVD0nMjkxLjg2JyBDVVJSRU5DWT0nRVVSJyAgRU1BSUw9J2N1c3RvbWVyQGV4YW1wbGUuY29tJz4KPEFERFJFU1MgQ09VTlRSWT0nVVNBJyBTVEFURT0nTlknIFpJUD0nMTAwMDEnIENJVFk9J05ZJyBTVFJFRVQ9JzY3NyBTVFJFRVQnIFBIT05FPSc4NzY5OTA5MCcgVFlQRT0nQklMTElORycvPgo8L09SREVSPg==&sha512=998150a2b27484b776a1628bfe7505a9cb430f276dfa35b14315c1c8f03381a90490f6608f0dcff789273e05926cd782e1bb941418a9673f43c47595aa7b8b0d";
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        js = (JavascriptExecutor) driver;
+        driver.manage().window().fullscreen();
     }
 
     @Test
     public void testUntitledTestCase() throws Exception {
-        driver.get("https://sandbox.cardpay.com/MI/payment.html?uuid=A2fg4F7CF18g0f4CG5c38F5B");
+        driver.get("baseUrl");
         driver.findElement(By.id("input-card-number")).click();
         driver.findElement(By.id("input-card-number")).clear();
         driver.findElement(By.id("input-card-number")).sendKeys("4000 0000 0000 0002");
         driver.findElement(By.id("input-card-holder")).click();
         driver.findElement(By.id("input-card-holder")).clear();
-        driver.findElement(By.id("input-card-holder")).sendKeys("SHALAMOV V");
+        driver.findElement(By.id("input-card-holder")).sendKeys("SHALAMOvV V");
         driver.findElement(By.id("card-expires-month")).click();
         new Select(driver.findElement(By.id("card-expires-month"))).selectByVisibleText("12");
         driver.findElement(By.id("card-expires-year")).click();
